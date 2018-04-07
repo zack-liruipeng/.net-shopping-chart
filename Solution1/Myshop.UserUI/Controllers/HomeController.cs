6dcd808dfe6a1526341e7f0edb0ce538d35a7509
zack-liruipeng <zack.liruipeng@gmail.com>
2018-04-07 15:12:46 -0400

    add image to our data base

--- Solution1/Myshop.UserUI/Controllers/HomeController.cs
+++ Solution1/Myshop.UserUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Myshop.Core.Models;
+using MyShop.Core.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,32 @@ namespace Myshop.UserUI.Controllers
 {
     public class HomeController : Controller
     {
+        IRepository<Product> context;
+        IRepository<ProductSection> productsections;
+
+        public HomeController(IRepository<Product> productcontext, IRepository<ProductSection> productsectioncontext)
+        {
+            context = productcontext;
+            productsections = productsectioncontext;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+        }
+
+        public ActionResult Details(string Id)
+        {
+            Product product = context.Find(Id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
         }
 
         public ActionResult About()
